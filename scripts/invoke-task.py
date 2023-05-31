1a7e2c799ec5bef3964b25c7056291b6e66b0804
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-05-31 16:52:44 -0400

    working, passes message to next queue

---
+++ scripts/invoke-task.py
@@ -0,0 +1,7 @@
+from celery import Celery
+
+app1 = Celery('tasks')
+app1.config_from_object('celeryconfig')
+
+#res = app1.send_task('tasks.tasks.send_to_dash', args=[{"hello":"world"}], kwargs={}, queue="etd_submission_ready")
+res = app1.send_task('etd-dash-service.tasks.send_to_dash', args=[{"hello":"world"}], kwargs={}, queue="etd_submission_ready")
