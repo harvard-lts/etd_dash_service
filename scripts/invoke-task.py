d3661f50b3f3b3c67da43f882ae85fdd2497a0d7
Chip Goines <chip_goines@harvard.edu>
2023-06-01 15:43:24 -0400

    trying to fix line indents for flake8

--- scripts/invoke-task.py
+++ scripts/invoke-task.py
@@ -6,4 +6,4 @@ app1.config_from_object('celeryconfig')
 # res = app1.send_task('tasks.tasks.send_to_dash', args=[{"hello":"world"}],
 #   kwargs={}, queue="etd_submission_ready")
 res = app1.send_task('etd-dash-service.tasks.send_to_dash',
-            args=[{"hello": "world"}], kwargs={}, queue="etd_submission_ready")
+                args=[{"hello": "world"}], kwargs={}, queue="etd_submission_ready")
