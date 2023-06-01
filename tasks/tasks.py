b29bd151b4a434624e8dcea01e90d5e4b8c05826
Chip Goines <chip_goines@harvard.edu>
2023-06-01 15:15:07 -0400

    flake8 fixes

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -4,11 +4,14 @@ import os
 app = Celery()
 app.config_from_object('celeryconfig')
 
-@app.task(serializer='json',name='etd-dash-service.tasks.send_to_dash')
+
+@app.task(serializer='json', name='etd-dash-service.tasks.send_to_dash')
 def send_to_dash(message):
-    message = {"Hello":"World"}
+    message = {"Hello": "World"}
     print("Hello World")
-    app.send_task("tasks.tasks.do_task", args=[message], kwargs={}, queue=os.getenv('NEXT_QUEUE_NAME'))
+    app.send_task("tasks.tasks.do_task", args=[message], kwargs={},
+      queue=os.getenv('NEXT_QUEUE_NAME'))
+
 
 if __name__ == '__main__':
     celery.start()
