e5b7ed81240b942c2f44ccfa811a73abecf7af3c
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-06-28 16:10:06 -0400

    use init for logging

--- etd/__init__.py
+++ etd/__init__.py
@@ -5,18 +5,26 @@ import os
 LOG_FILE_BACKUP_COUNT = os.getenv('LOG_FILE_BACKUP_COUNT')
 LOG_ROTATION = "midnight"
 
+
 def configure_logger():
     log_level = os.getenv("APP_LOG_LEVEL", "WARNING")
-    log_file_path = os.getenv("LOGFILE_PATH", "/home/etdadm/logs/etd/etd_dash.log")
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    
-    file_handler = TimedRotatingFileHandler(
-        filename=log_file_path,
-        when=LOG_ROTATION,
-        backupCount=LOG_FILE_BACKUP_COUNT
-    )
+    log_file_path = os.getenv("LOGFILE_PATH",
+                              "/home/etdadm/logs/etd/etd_dash.log")
+    formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
     logger = logging.getLogger('etd_dash')
-        
-    logger.addHandler(file_handler)
-    file_handler.setFormatter(formatter)
-    logger.setLevel(log_level)   
+    logger.addHandler(console_handler)
+    if not os.getenv("CONSOLE_LOGGING_ONLY"):
+        file_handler = TimedRotatingFileHandler(
+            filename=log_file_path,
+            when=LOG_ROTATION,
+            backupCount=LOG_FILE_BACKUP_COUNT
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    logger.setLevel(log_level)
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,14 +1,13 @@
 from celery import Celery
 import os
 import logging
-#from etd.logging_config import configure_logger
 import etd
 
 app = Celery()
 app.config_from_object('celeryconfig')
 etd.configure_logger()
 logger = logging.getLogger('etd_dash')
-#app.log.setup(logger=celery_logger)
+
 
 @app.task(serializer='json', name='etd-dash-service.tasks.send_to_dash')
 def send_to_dash(message):
@@ -17,5 +16,3 @@ def send_to_dash(message):
     new_message = {"hello": "from etd-dash-service"}
     app.send_task("etd-alma-service.tasks.send_to_alma", args=[new_message],
                   kwargs={}, queue=os.getenv('PUBLISH_QUEUE_NAME'))
-
-           
