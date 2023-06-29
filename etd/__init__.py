e636990654659ee9dec3641114c7de00cfd8b1fc
michael-lts <michael_vandermillen@harvard.edu>
2023-06-29 15:30:43 -0400

    Etd 185 (#11)
    * initial logging functionalit
    
    * add loging from init
    
    * use init for logging
    
    * change log file path
    
    * fix python version, 100% test coverage
    
    * exclude init from test coverage

---
+++ etd/__init__.py
@@ -0,0 +1,30 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+
+LOG_FILE_BACKUP_COUNT = os.getenv('LOG_FILE_BACKUP_COUNT')
+LOG_ROTATION = "midnight"
+
+
+def configure_logger():  # pragma: no cover
+    log_level = os.getenv("APP_LOG_LEVEL", "WARNING")
+    log_file_path = os.getenv("LOGFILE_PATH",
+                              "/home/etdadm/logs/etd_dash/etd_dash.log")
+    formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger('etd_dash')
+    logger.addHandler(console_handler)
+    if not os.getenv("CONSOLE_LOGGING_ONLY"):
+        file_handler = TimedRotatingFileHandler(
+            filename=log_file_path,
+            when=LOG_ROTATION,
+            backupCount=LOG_FILE_BACKUP_COUNT
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    logger.setLevel(log_level)
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,14 +1,18 @@
 from celery import Celery
 import os
+import logging
+import etd
 
 app = Celery()
 app.config_from_object('celeryconfig')
+etd.configure_logger()
+logger = logging.getLogger('etd_dash')
 
 
 @app.task(serializer='json', name='etd-dash-service.tasks.send_to_dash')
 def send_to_dash(message):
-    print("message")
-    print(message)
+    logger.info("message")
+    logger.info(message)
     new_message = {"hello": "from etd-dash-service"}
     app.send_task("etd-alma-service.tasks.send_to_alma", args=[new_message],
                   kwargs={}, queue=os.getenv('PUBLISH_QUEUE_NAME'))
