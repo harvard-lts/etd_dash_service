6b3c240221d0283cac54656597c1f6b309577e4b
vac765 <valdeva_crema@harvard.edu>
2023-06-29 20:20:32 -0400

    feature flag implementation

--- scripts/invoke-task.py
+++ scripts/invoke-task.py
@@ -1,9 +1,17 @@
 from celery import Celery
 import os
+import json
 
 app1 = Celery('tasks')
 app1.config_from_object('celeryconfig')
 
+arguments = {"hello": "world", "feature_flags": {
+            'dash_feature_flag': "off",
+            'alma_feature_flag': "off",
+            'send_to_drs_feature_flag': "off",
+            'drs_holding_record_feature_flag': "off"}}
+json_args = json.dumps(arguments)
+
 res = app1.send_task('etd-dash-service.tasks.send_to_dash',
-                     args=[{"hello": "world"}], kwargs={},
+                     args=[json_args], kwargs={},
                      queue=os.getenv("CONSUME_QUEUE_NAME"))
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import os
+import json
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -9,6 +10,19 @@ app.config_from_object('celeryconfig')
 def send_to_dash(message):
     print("message")
     print(message)
+    json_message = json.loads(message)
     new_message = {"hello": "from etd-dash-service"}
+    if "feature_flags" in json_message:
+        feature_flags = json_message["feature_flags"]
+        new_message["feature_flags"] = feature_flags
+        if "dash_feature_flag" in feature_flags and \
+                feature_flags["dash_feature_flag"] == "on":
+            # Send to DASH
+            print("FEATURE IS ON>>>>>SEND TO DASH")
+        else:
+            # Feature is off so do hello world
+            print("FEATURE FLAGS FOUND")
+            print(json_message['feature_flags'])
+
     app.send_task("etd-alma-service.tasks.send_to_alma", args=[new_message],
                   kwargs={}, queue=os.getenv('PUBLISH_QUEUE_NAME'))
