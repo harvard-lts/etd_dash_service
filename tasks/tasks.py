eb1a68ed1aea66673dda9b70cbf68d169fa2781d
vac765 <valdeva_crema@harvard.edu>
2023-06-29 20:20:32 -0400

    feature flag implementation

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 import os
 import logging
 import etd
+import json
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -13,6 +14,19 @@ logger = logging.getLogger('etd_dash')
 def send_to_dash(message):
     logger.info("message")
     logger.info(message)
+    json_message = json.loads(message)
     new_message = {"hello": "from etd-dash-service"}
+    if "feature_flags" in json_message:
+        feature_flags = json_message["feature_flags"]
+        new_message["feature_flags"] = feature_flags
+        if "dash_feature_flag" in feature_flags and \
+                feature_flags["dash_feature_flag"] == "on":
+            # Send to DASH
+            logger.debug("FEATURE IS ON>>>>>SEND TO DASH")
+        else:
+            # Feature is off so do hello world
+            logger.debug("FEATURE FLAGS FOUND")
+            logger.debug(json_message['feature_flags'])
+
     app.send_task("etd-alma-service.tasks.send_to_alma", args=[new_message],
                   kwargs={}, queue=os.getenv('PUBLISH_QUEUE_NAME'))
