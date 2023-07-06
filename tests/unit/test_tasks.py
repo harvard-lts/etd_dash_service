484ab6fba6467152630a69512712c759dfeed64d
vac765 <valdeva_crema@harvard.edu>
2023-07-05 20:42:40 -0400

    unit test

---
+++ tests/unit/test_tasks.py
@@ -0,0 +1,18 @@
+import tasks.tasks as tasks
+import json
+
+FEATURE_FLAGS = "feature_flags"
+
+
+class TestTasksClass():
+
+    def test_send_to_dash(self):
+        message = {"unit_test": "true", FEATURE_FLAGS: {
+                'dash_feature_flag': "off",
+                'alma_feature_flag': "off",
+                'send_to_drs_feature_flag': "off",
+                'drs_holding_record_feature_flag': "off"}}
+        json_args = json.dumps(message)
+        retval = tasks.send_to_dash(json_args)
+        assert "hello" in retval
+        assert "feature_flags" in retval
